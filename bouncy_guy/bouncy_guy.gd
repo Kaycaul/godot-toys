5c00217ade1b1c76fa89b61ca02fbd48218ffb2f
Doeball <doughcatball@gmail.com>
2025-04-04 00:17:27 -0400

    wolp

--- bouncy_guy/bouncy_guy.gd
+++ bouncy_guy/bouncy_guy.gd
@@ -1,4 +1,22 @@
 extends RigidBody2D
 
-func _process(delta: float) -> void:
-	pass
+@export var move_force : float = 0.0
+
+var wolp_particle : Resource = preload("uid://cl35vab8dlcdw")
+
+func _process(_delta: float) -> void:
+	move()
+	explode()
+
+func explode() -> void:
+	if Input.is_physical_key_pressed(KEY_SPACE):
+		var new_guy = wolp_particle.instantiate()
+
+func move() -> void:
+	# get direction input vector
+	var input : Vector2 = Vector2.ZERO
+	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input.y = Input.get_action_strength("move_back") - Input.get_action_strength("move_forward")
+	input = input.normalized()
+	# apply impulse in that direction
+	apply_impulse(input * move_force)
