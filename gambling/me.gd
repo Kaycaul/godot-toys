40974246ef05e40be5f45577b28cb037346ce9e7
Doeball <doughcatball@gmail.com>
2025-05-01 02:39:35 -0400

    woah uniforms

--- gambling/me.gd
+++ gambling/me.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
 @export var move_force : float = 0.0
+@onready var sprite : Sprite2D = $Sneakers
 
 func _process(_delta: float) -> void:
 	move(_delta)
+	update_shader(_delta)
 
 func move(delta : float) -> void:
 	# get direction input vector
@@ -13,3 +15,12 @@ func move(delta : float) -> void:
 	input = input.normalized()
 	# apply impulse in that direction
 	apply_impulse(input * move_force * delta * 144)
+
+func update_shader(_delta : float) -> void:
+	sprite.material.set_shader_parameter("wave_offset", position * 0.1)
+
+func set_redness_amount(redness : float) -> void:
+	sprite.material.set_shader_parameter("red_amount", redness)
+
+func become_not_red() -> void:
+	set_redness_amount(0.0)
