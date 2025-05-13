d71433c69c9bebcdf31e98969ac966d52dc8e897
Doeball <doughcatball@gmail.com>
2025-05-13 05:21:48 -0400

    loud

--- clicker/collector.gd
+++ clicker/collector.gd
@@ -2,8 +2,14 @@ extends Area2D
 
 signal thing_collected
 
+@onready var scream_player : AudioStreamPlayer = %"Scream Player"
+
 func _on_body_entered(body: Node2D) -> void:
 	var wega = body as Wega
 	if not wega: return
 	wega.queue_free()
 	thing_collected.emit()
+
+func play_scream_sound() -> void:
+	scream_player.pitch_scale = randf_range(0.95, 1.05)
+	scream_player.play()
