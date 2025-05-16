9807d4093ed2b9989b8bc92b5ee513e44b396001
Doeball <doughcatball@gmail.com>
2025-05-15 23:21:26 -0400

    i forgot to commit last night and i dont remember what i did

--- shader_test/soup.gd
+++ shader_test/soup.gd
@@ -6,20 +6,22 @@ extends Area2D
 @export var explosion_particle_lifetime : float = 1.0
 @export var lifetime_randomness : float = 0.0
 
-var soup_particle : Resource = preload("uid://03wpfuvr77js")
+#var soup_particle : Resource = preload("uid://03wpfuvr77js")
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
 
 func spawn_explosion() -> void:
-	for i in range(explosion_particle_count):
-		var new_soup_particle : SoupParticle = soup_particle.instantiate()
-		new_soup_particle.position = position
-		# pick a random direction to rotate it towards, then push it
-		var random_percentage : float = randf_range(-explosion_randomness, explosion_randomness)
-		var random_force : float = explosion_force * (1 + random_percentage)
-		var random_lifetime : float = explosion_particle_lifetime * (1 + random_percentage)
-		var random_angle : float = randf_range(0, 2 * PI)
-		var random_direction : Vector2 = Vector2(cos(random_angle), sin(random_angle))
-		new_soup_particle.rotation = random_angle
-		new_soup_particle.apply_central_impulse(random_direction * random_force)
-		new_soup_particle.set_lifetime(random_lifetime)
-		# add to tree
-		call_deferred("add_sibling", new_soup_particle)
+	#for i in range(explosion_particle_count):
+		#var new_soup_particle : SoupParticle = soup_particle.instantiate()
+		#new_soup_particle.position = position
+		## pick a random direction to rotate it towards, then push it
+		#var random_percentage : float = randf_range(-explosion_randomness, explosion_randomness)
+		#var random_force : float = explosion_force * (1 + random_percentage)
+		#var random_lifetime : float = explosion_particle_lifetime * (1 + random_percentage)
+		#var random_angle : float = randf_range(0, 2 * PI)
+		#var random_direction : Vector2 = Vector2(cos(random_angle), sin(random_angle))
+		#new_soup_particle.rotation = random_angle
+		#new_soup_particle.apply_central_impulse(random_direction * random_force)
+		#new_soup_particle.set_lifetime(random_lifetime)
+		## add to tree
+		#call_deferred("add_sibling", new_soup_particle)
+	gpu_particles_2d.emitting = true
