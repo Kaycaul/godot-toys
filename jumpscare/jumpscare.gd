e68b2dbdb508c7253574cecaec95713d8fe188d9
Doeball <doughcatball@gmail.com>
2025-05-25 21:39:21 -0400

    exploding glass sounds

--- jumpscare/jumpscare.gd
+++ jumpscare/jumpscare.gd
@@ -1,16 +1,32 @@
 extends Node2D
 
-# TODO: count how many successful shots the player gets on the jumpscare and display score on hud
 # TODO: make him expand or something to indicate how long you have to shoot
 # TODO: end the game (and restart) when you miss your window to shoot
-# TODO: glass shattering sound when you kill him
 # TODO: define game speed scale with a literal difficulty curve
 # TODO: overly dramatic synthwave music or something lmao
+
+const GLASS_EXPLODE_1 : AudioStreamMP3 = preload("uid://b2q6blgrn1r5s")
+const GLASS_EXPLODE_2 : AudioStreamMP3 = preload("uid://bkpeht7vfo72")
+const GLASS_EXPLODE_3 : AudioStreamMP3 = preload("uid://d1bkem0ju4b3u")
+const GLASS_EXPLODE_4 : AudioStreamMP3 = preload("uid://dwwn3v3umc3vp")
+const GLASS_EXPLODE_5 : AudioStreamMP3 = preload("uid://r0rvyfhefwcx")
+const GLASS_EXPLODE_6 : AudioStreamMP3 = preload("uid://dq3xd7hhe8niy")
+const GLASS_EXPLODE_7 : AudioStreamMP3 = preload("uid://rw7o60tsboxk")
+const EXPLODE_SOUNDS = [
+	GLASS_EXPLODE_1, 
+	GLASS_EXPLODE_2, 
+	GLASS_EXPLODE_3,
+	GLASS_EXPLODE_4,
+	GLASS_EXPLODE_5,
+	GLASS_EXPLODE_6,
+	GLASS_EXPLODE_7
+]
 
 @onready var sneaks_scare: SneaksScare = %sneaks_scare
 @onready var space_prompt: Label = %space_prompt
 @onready var gun: Gun = %gun
 @onready var score_label: Label = %ScoreLabel
+@onready var kill_sound: AudioStreamPlayer = %KillSound
 
 var on_hit_particle_explosion : Resource = preload("uid://bfe05b5oab0la")
 
@@ -45,11 +61,17 @@ func shoot() -> void:
 	if displaying_jumpscare:
 		shoot_jumpscare()
 
+## called on a successful hit on the enemy
 func shoot_jumpscare() -> void:
-		hide_jumpscare()
-		spawn_explosion()
-		gain_points(1)
-		multiply_game_speed(1.025)
+	hide_jumpscare()
+	spawn_explosion()
+	gain_points(1)
+	multiply_game_speed(1.025)
+	play_random_hit_sound()
+
+func play_random_hit_sound() -> void:
+	kill_sound.stream = EXPLODE_SOUNDS.pick_random()
+	kill_sound.play()
 
 func gain_points(points: int) -> void:
 	score += points
