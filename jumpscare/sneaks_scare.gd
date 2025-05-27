aa24fc39048960ba84bb033708ad4fdb2ebbaf24
Doeball <doughcatball@gmail.com>
2025-05-26 22:31:23 -0400

    scream streching

--- jumpscare/sneaks_scare.gd
+++ jumpscare/sneaks_scare.gd
@@ -11,15 +11,33 @@ signal on_jumpscare
 
 var cooldown : float
 var speed_scale := 1.0
+var displaying_jumpscare := false
 
 func _ready() -> void:
 	jumpscare_timer.timeout.connect(activate_jumpscare)
 	cooldown = initial_cooldown
 	reset_jumpscare()
 
+func _process(delta : float) -> void:
+	widen_while_visible(delta)
+
+var time_since_jumpscare_start := 0.0
+@onready var original_width_scale := sprite.scale.x
+func widen_while_visible(delta : float) -> void:
+	if not displaying_jumpscare: return
+	var true_scream_duration := scream.stream.get_length() / speed_scale
+	var scream_sound_progress := time_since_jumpscare_start / true_scream_duration
+	# TODO: transform `scream_sound_progress` here with a [0,1]->[0,1] curve
+	var new_width_scale := original_width_scale * (1 + scream_sound_progress)
+	sprite.scale.x = new_width_scale
+	time_since_jumpscare_start += delta
+
 func reset_jumpscare() -> void:
+	displaying_jumpscare = false
 	sprite.hide()
 	scream.stop()
+	# reset stretching visual
+	sprite.scale.x = original_width_scale
 
 func begin_scare_timer(new_cooldown: float = -1.0) -> void:
 	if new_cooldown > 0.0: cooldown = new_cooldown
@@ -30,6 +48,7 @@ func begin_scare_timer(new_cooldown: float = -1.0) -> void:
 
 func activate_jumpscare() -> void:
 	on_jumpscare.emit()
+	displaying_jumpscare = true
 	# reveal sprite
 	sprite.material.set_shader_parameter("flashing", true)
 	sprite.material.set_shader_parameter("wega_color", true)
@@ -37,6 +56,9 @@ func activate_jumpscare() -> void:
 	# play scream sound
 	scream.pitch_scale = randf_range(0.96, 1.04) * speed_scale
 	scream.play()
+	# cache the initial width so it can be reset later
+	original_width_scale = sprite.scale.x
+	time_since_jumpscare_start = 0.0
 
 func update_speed_scale(new_speed_scale: float) -> void:
 	speed_scale = new_speed_scale
