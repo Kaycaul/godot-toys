585f894495c1bea0cf80411d65ae9cfbcfc94131
Doeball <doughcatball@gmail.com>
2025-06-04 23:43:53 -0400

    tweaking platforming parameters

--- weeknd/player.gd
+++ weeknd/player.gd
@@ -9,6 +9,11 @@ func _process(delta: float) -> void:
 	var input : Vector2 = Vector2.ZERO
 	input.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	apply_impulse(input * move_force * delta)
+	# reduce friction only while moving
+	if input.x > 0 or input.x < 0:
+		physics_material_override.friction = 0.05
+	else:
+		physics_material_override.friction = 0.70
 	# jump
 	if Input.is_action_just_pressed("jump"):
 		apply_central_impulse(Vector2.UP * jump_force)
